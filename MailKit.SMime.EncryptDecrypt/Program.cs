874ef235080dbdda317abb88e94a57cadc0b03ad
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2018-08-15 09:36:59 +0200

    Add project files.

---
+++ MailKit.SMime.EncryptDecrypt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MailKit.SMime.EncryptDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
