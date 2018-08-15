fd3e7be354b79af4e75fbdaca17ef23586153a11
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2018-08-15 12:34:38 +0200

    Setup.

--- MailKit.SMime.EncryptDecrypt/Program.cs
+++ MailKit.SMime.EncryptDecrypt/Program.cs
@@ -2,11 +2,11 @@
 
 namespace MailKit.SMime.EncryptDecrypt
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
