7c6ed357a1675d223b8875c2828311b816cbc54b
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2018-08-15 14:46:20 +0200

    Initial work.

---
+++ MailKit.SMime.EncryptDecrypt/SmtpProperties.cs
@@ -0,0 +1,13 @@
+﻿namespace MailKit.SMime.EncryptDecrypt
+{
+    public class SmtpProperties
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int SmtpPort { get; set; }
+        public string EmailAddress { get; set; }
+        public bool UseAuth { get; set; }
+        public bool UseSsl { get; set; }
+    }
+}
---
+++ MailKit.SMime.EncryptDecrypt/SmtpSender.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace MailKit.SMime.EncryptDecrypt
+{
+    public class SmtpSender
+    {
+
+        private static async Task Send(MimeMessage message, SmtpProperties props)
+        {
+            var option = SecureSocketOptions.Auto;
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(props.Host, props.SmtpPort, option);
+                if (props.UseAuth)
+                {
+                    await client.AuthenticateAsync(props.UserName, props.Password);
+                }
+
+                await client.SendAsync(message);
+
+                client.Disconnect(true);
+            }
+        }
+    }
+}
