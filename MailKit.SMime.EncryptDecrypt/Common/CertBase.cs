f8cda28faa1a2c7cb21f98b40d531071db0d5d7e
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2018-08-16 09:20:45 +0200

    Refactoring and updating.

---
+++ MailKit.SMime.EncryptDecrypt/Common/CertBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using MimeKit.Cryptography;
+
+namespace MailKit.SMime.EncryptDecrypt.Common
+{
+    public class CertBase
+    {
+        private static SqliteCertificateDatabase Database { get; set; }
+
+        public static IX509CertificateDatabase OpenDatabase(string fileName)
+        {
+            var builder = new SQLiteConnectionStringBuilder
+            {
+                DateTimeFormat = SQLiteDateFormats.Ticks,
+                DataSource = fileName
+            };
+
+            if (!File.Exists(fileName))
+                SQLiteConnection.CreateFile(fileName);
+
+            var sqlite = new SQLiteConnection(builder.ConnectionString);
+            sqlite.Open();
+
+            Database = new SqliteCertificateDatabase(sqlite, "password");
+
+            return Database;
+        }
+    }
+}
